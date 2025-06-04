8ae6ea2d16886ec58471b44bed1030a4c8394664
amitan12345 <03_habitat.shelter@icloud.com>
2025-06-04 19:51:32 +0900

    feat: add Job module migrations

---
+++ Modules/Job/app/Models/Company.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Modules\Job\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+// use Modules\Job\Database\Factories\CompanyFactory;
+
+class Company extends Model
+{
+    use HasFactory;
+
+    /**
+     * The attributes that are mass assignable.
+     */
+    protected $fillable = [];
+
+    // protected static function newFactory(): CompanyFactory
+    // {
+    //     // return CompanyFactory::new();
+    // }
+}
